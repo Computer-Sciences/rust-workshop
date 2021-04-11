0e2a629aae461dcd1da939eab09a808f5fbc8424
Florian GOTO <you@example.com>
2021-04-11 22:52:50 +0200

    integer data types

--- hi/src/main.rs
+++ hi/src/main.rs
@@ -1,11 +1,13 @@
 fn main() {
     // variables are immutable by default
-    let x = 20;
-    println!("x is {}",x);
+    let x = -20; // default inferred type is i32
+    println!("x is {}", x);
 
-    let mut local_var = 20;
-    println!("local_var is {}",local_var);
-
+    let mut local_var: u8 = 20;
+    println!("local_var is {}", local_var);
     local_var = 50;
-    println!("local_var is {}",local_var);
+    println!("local_var is {}", local_var);
+
+    let a_num: u16 = 2021;
+    println!("a_num is {}", a_num);
 }
