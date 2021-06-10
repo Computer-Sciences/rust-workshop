222425e7e5174a2e4265e81ad519d18324e1f2da
Florian GOTO <you@example.com>
2021-06-10 19:47:22 +0200

    slices as function params

---
+++ slices_as_fn_params/target/debug/deps/slices_as_fn_params-d565a4a5111ea9c9.d
@@ -0,0 +1,5 @@
+/home/my/workspace/rust-workshop/slices_as_fn_params/target/debug/deps/slices_as_fn_params-d565a4a5111ea9c9: src/main.rs
+
+/home/my/workspace/rust-workshop/slices_as_fn_params/target/debug/deps/slices_as_fn_params-d565a4a5111ea9c9.d: src/main.rs
+
+src/main.rs:
---
+++ slices_as_fn_params/target/debug/slices_as_fn_params.d
@@ -0,0 +1,1 @@
+/home/my/workspace/rust-workshop/slices_as_fn_params/target/debug/slices_as_fn_params: /home/my/workspace/rust-workshop/slices_as_fn_params/src/main.rs
