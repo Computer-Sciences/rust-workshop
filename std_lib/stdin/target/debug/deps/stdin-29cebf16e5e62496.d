11b715a92d3f471ba76b3b82994d9767bf64faac
Florian GOTO <you@example.com>
2021-06-12 22:10:03 +0200

    parsing standard nput

---
+++ std_lib/stdin/target/debug/deps/stdin-29cebf16e5e62496.d
@@ -0,0 +1,5 @@
+/home/my/workspace/rust-workshop/std_lib/stdin/target/debug/deps/stdin-29cebf16e5e62496: src/main.rs
+
+/home/my/workspace/rust-workshop/std_lib/stdin/target/debug/deps/stdin-29cebf16e5e62496.d: src/main.rs
+
+src/main.rs:
---
+++ std_lib/stdin/target/debug/stdin.d
@@ -0,0 +1,1 @@
+/home/my/workspace/rust-workshop/std_lib/stdin/target/debug/stdin: /home/my/workspace/rust-workshop/std_lib/stdin/src/main.rs
